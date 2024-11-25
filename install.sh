38646ed917e78def36aa07cb39303bd2d5614c4b
Shadow-Dev <Onemoment1234567@outlook.com>
2024-11-25 21:23:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-
+echo Starting Installation...
+echo Installing git
+sudo pacman -S git 
+clear
+git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/Arch-Hyprland
+cd ~/Arch-Hyprland
+chmod +x install.sh
+./install.sh
