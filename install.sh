c8cd35462f24fe80e3fab2cf98f8e329cf58a41c
Shadow-Dev <Onemoment1234567@outlook.com>
2024-11-26 18:34:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-echo Starting Installation...
-echo Installing git(Already installed? select "N" in the reinstall)
-sudo pacman -S git 
-clear
+echo Starting Installation...[From "https://github.com/JaKooLit/Arch-Hyprland"]
+sleep 1
+echo Cloning Jakoolit Arch_Hyprland
 git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/Arch-Hyprland
 cd ~/Arch-Hyprland
 chmod +x install.sh
